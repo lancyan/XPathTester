696b962b0d927f96d8f84724dfdd9db33f23148f
lancyan <lancyan@163.com>
2017-11-26 18:09:51 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace XPathTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.树形结构ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.文本结构ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.innerHTMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.innerTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.matchSingleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matchMultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.IgnoreWhiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusMsg = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox3);
+            this.splitContainer1.Panel2.Controls.Add(this.treeView1);
+            this.splitContainer1.Size = new System.Drawing.Size(902, 632);
+            this.splitContainer1.SplitterDistance = 457;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.richTextBox2);
+            this.splitContainer2.Size = new System.Drawing.Size(902, 457);
+            this.splitContainer2.SplitterDistance = 177;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(902, 177);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(902, 276);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox3.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(902, 171);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.树形结构ToolStripMenuItem,
+            this.文本结构ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 48);
+            // 
+            // 树形结构ToolStripMenuItem
+            // 
+            this.树形结构ToolStripMenuItem.Name = "树形结构ToolStripMenuItem";
+            this.树形结构ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.树形结构ToolStripMenuItem.Text = "树形结构";
+            this.树形结构ToolStripMenuItem.Click += new System.EventHandler(this.树形结构ToolStripMenuItem_Click);
+            // 
+            // 文本结构ToolStripMenuItem
+            // 
+            this.文本结构ToolStripMenuItem.Name = "文本结构ToolStripMenuItem";
+            this.文本结构ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.文本结构ToolStripMenuItem.Text = "文本结构";
+            this.文本结构ToolStripMenuItem.Click += new System.EventHandler(this.文本结构ToolStripMenuItem_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(902, 171);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(902, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.innerHTMLToolStripMenuItem,
+            this.innerTextToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(53, 22);
+            this.toolStripDropDownButton1.Text = " 运行 ";
+            this.toolStripDropDownButton1.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStripDropDownButton1_DropDownItemClicked);
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // innerHTMLToolStripMenuItem
+            // 
+            this.innerHTMLToolStripMenuItem.Checked = true;
+            this.innerHTMLToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.innerHTMLToolStripMenuItem.Name = "innerHTMLToolStripMenuItem";
+            this.innerHTMLToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.innerHTMLToolStripMenuItem.Text = "InnerHTML";
+            // 
+            // innerTextToolStripMenuItem
+            // 
+            this.innerTextToolStripMenuItem.Name = "innerTextToolStripMenuItem";
+            this.innerTextToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.innerTextToolStripMenuItem.Text = "InnerText";
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.matchSingleToolStripMenuItem,
+            this.matchMultToolStripMenuItem,
+            this.IgnoreWhiteToolStripMenuItem});
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(69, 22);
+            this.toolStripDropDownButton2.Text = "匹配选项";
+            this.toolStripDropDownButton2.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStripDropDownButton2_DropDownItemClicked);
+            // 
+            // matchSingleToolStripMenuItem
+            // 
+            this.matchSingleToolStripMenuItem.Checked = true;
+            this.matchSingleToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.matchSingleToolStripMenuItem.Name = "matchSingleToolStripMenuItem";
+            this.matchSingleToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.matchSingleToolStripMenuItem.Text = "匹配单节点";
+            // 
+            // matchMultToolStripMenuItem
+            // 
+            this.matchMultToolStripMenuItem.Name = "matchMultToolStripMenuItem";
+            this.matchMultToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.matchMultToolStripMenuItem.Text = "匹配多节点";
+            // 
+            // IgnoreWhiteToolStripMenuItem
+            // 
+            this.IgnoreWhiteToolStripMenuItem.Checked = true;
+            this.IgnoreWhiteToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.IgnoreWhiteToolStripMenuItem.Name = "IgnoreWhiteToolStripMenuItem";
+            this.IgnoreWhiteToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.IgnoreWhiteToolStripMenuItem.Text = "忽略空白节点";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusMsg});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 657);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(902, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusMsg
+            // 
+            this.toolStripStatusMsg.Name = "toolStripStatusMsg";
+            this.toolStripStatusMsg.Size = new System.Drawing.Size(64, 17);
+            this.toolStripStatusMsg.Text = " xPath 1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 679);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "XPath Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusMsg;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem innerHTMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem innerTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripMenuItem matchSingleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matchMultToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 树形结构ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 文本结构ToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ToolStripMenuItem IgnoreWhiteToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace XPathTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        bool isIgnoreWhite = false;
+
+        private void Run()
+        {
+            string xpath = "";
+            string source = "";
+            string result = "";
+
+            treeView1.Nodes.Clear();
+            richTextBox3.Clear();
+
+            isIgnoreWhite = IgnoreWhiteToolStripMenuItem.Checked;
+
+            xpath = richTextBox1.Text.Trim();
+            source = richTextBox2.Text.Trim();
+            //result = richTextBox3.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(xpath))
+            {
+                toolStripStatusMsg.Text = "xpath不能为空！！";
+                return;
+            }
+            if (xpath.EndsWith("/"))
+            {
+                toolStripStatusMsg.Text = "xpath不能以/结尾！！";
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                toolStripStatusMsg.Text = "匹配内容不能为空！！";
+                return;
+            }
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(source);
+
+            HtmlNode rootnode = doc.DocumentNode;
+            try
+            {
+                bool isHtml = innerHTMLToolStripMenuItem.Checked;
+                if (matchSingleToolStripMenuItem.Checked)
+                {
+                    DoSingle(rootnode, xpath, isHtml);
+                }
+                else
+                {
+                    DoMult(rootnode, xpath, isHtml);
+                }
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusMsg.Text = ex.Message;
+            }
+        }
+
+
+        private void CreateTree(HtmlNode rootNode, TreeNode tn, bool flag)
+        {
+            if (rootNode.HasChildNodes)
+            {
+                foreach (var node in rootNode.ChildNodes)
+                {
+                    string tagName = node.Name;
+                    string str = flag ? node.OuterHtml : node.InnerText;
+                    if (isIgnoreWhite)
+                    {
+                        if (!(tagName == "#text" && string.IsNullOrWhiteSpace(node.InnerText.Trim())))
+                        {
+                            //loopNode(tagName, str, node, flag);
+                            TreeNode newNode = new TreeNode(tagName);
+                            if (tagName == "#text")
+                            {
+                                newNode.Text = newNode.Text + "  " + str.Trim();
+                            }
+                            newNode.Tag = node.StreamPosition + ":" + node.OuterHtml.Length;
+                            tn.Nodes.Add(newNode);
+                            CreateTree(node, newNode,flag);
+                        }
+                    }
+                    else
+                    {
+                        //loopNode(tagName, str, child, flag);
+                        TreeNode newNode = new TreeNode(tagName);
+                        if (tagName == "#text")
+                        {
+                            newNode.Text = newNode.Text + "  " + str.Trim();
+                        }
+                        newNode.Tag = node.StreamPosition + ":" + node.OuterHtml.Length;
+                        tn.Nodes.Add(newNode);
+                        CreateTree(node, newNode,flag);
+                    }
+                }
+            }
+        }
+
+        private void DoSingle(HtmlNode rootNode, string xPath, bool flag)
+        {
+            HtmlNode node = rootNode.SelectSingleNode(xPath);
+            if (node != null)
+            {
+                string str = flag ? node.OuterHtml : node.InnerText;
+                richTextBox3.AppendText(str + "\r\n");
+                if (flag)
+                {
+                    string tagName = node.Name;
+                    if (isIgnoreWhite)
+                    {
+                        if (!(tagName == "#text" && string.IsNullOrWhiteSpace(str.Trim())))
+                        {
+                            loopNode(tagName, str, node,flag);
+                        }
+                    }
+                    else
+                    {
+                        loopNode(tagName, str, node, flag);
+                    }
+                }
+            }
+        }
+        private void DoMult(HtmlNode rootNode, string xPath, bool flag)
+        {
+            HtmlNodeCollection nodes = rootNode.SelectNodes(xPath);
+            if (nodes != null && nodes.Count > 0)
+            {
+                foreach (var node in nodes)
+                {
+                    string str = flag ? node.OuterHtml : node.InnerText;
+                    richTextBox3.AppendText(str + "\r\n");
+
+                    string tagName = node.Name;
+                    if (isIgnoreWhite)
+                    {
+                        if (!(tagName == "#text" && string.IsNullOrWhiteSpace(str.Trim())))
+                        {
+                            loopNode(tagName, str, node, flag);
+                        }
+                    }
+                    else
+                    {
+                        loopNode(tagName, str, node, flag);
+                    }
+
+                }
+            }
+
+        }
+        private void loopNode(string tagName, string str, HtmlNode node, bool flag)
+        {
+            TreeNode newNode = new TreeNode(tagName);
+            if (tagName == "#text")
+            {
+                newNode.Text = newNode.Text + "  " + str.Trim();
+            }
+            newNode.Tag = node.StreamPosition + ":" + node.OuterHtml.Length;
+            treeView1.Nodes.Add(newNode);
+            CreateTree(node, newNode, flag);
+        }
+
+     
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                Run();
+            }
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+            Run();
+        }
+
+        private void 树形结构ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.Visible = true;
+            richTextBox3.Visible = false;
+        }
+
+        private void 文本结构ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.Visible = false;
+            richTextBox3.Visible = true;
+        }
+
+  
+
+        private void toolStripDropDownButton1_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            foreach (ToolStripMenuItem mi in toolStripDropDownButton1.DropDownItems)
+            {
+                mi.Checked = false;
+            }
+            ((ToolStripMenuItem)e.ClickedItem).Checked = true;
+        }
+
+        private void toolStripDropDownButton2_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            bool isChecked = ((ToolStripMenuItem)e.ClickedItem).Checked;
+            ((ToolStripMenuItem)e.ClickedItem).Checked = !isChecked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ClearSelection(RichTextBox rtb)
+        {
+            if (rtb.Text.Length > 0)
+            {
+                int currentIndex = rtb.SelectionStart;
+                rtb.SelectAll();
+                rtb.SelectionBackColor = Color.White;
+                rtb.SelectionColor = SystemColors.WindowText;
+                rtb.SelectionLength = 0;
+                rtb.SelectionStart = currentIndex;
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode node = e.Node;
+            if (node.Tag != null)
+            {
+                int[] arr = Array.ConvertAll(node.Tag.ToString().Split(':'), int.Parse);
+                ClearSelection(richTextBox2);
+                richTextBox2.Select(arr[0], arr[1]);
+                richTextBox2.SelectionColor = Color.White;
+                richTextBox2.SelectionBackColor = Color.Red;
+                richTextBox2.ScrollToCaret();
+            }
+        }
+    }
+}
